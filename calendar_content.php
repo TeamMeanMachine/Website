44bb03f789b0be039a9b4a295e4944f2d570ef91
HiceS <shawnhice@gmail.com>
2015-05-30 10:13:59 -0700

    Fixed Calendar
    Added the appropriate changes in the change log, this was a error due to
    the new google reformating coming from a product release at IO I would
    expect. This was checked cross 2 servers for compatibility and error
    analysis and I can conclude it was directly the hosts fault and not the
    developers.

---
+++ calendar_content.php
@@ -0,0 +1,15 @@
+			<div class="post">
+				<h1 class="title"><a href="#">Calendar</a></h1>
+				<div class="entry">
+<!--					<iframe src="http://www.google.com/calendar/embed?src=chsfirst%40gmail.com&amp;ctz=America/Los_Angeles" style="border: 0;" frameborder="0" height="550" scrolling="no" width="100%"></iframe>-->
+             <!--       <iframe src="jquery/restylegc/restylegc.php?src=chsfirst%40gmail.com&color=%234E5D6C&showTitle=0&showTabs=0&showCalendars=0" style="border: 0;" frameborder="0" height="550px" scrolling="no" width="105%"></iframe> -->
+                    <iframe src="https://www.google.com/calendar/embed?height=550&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=chsfirst%40gmail.com&amp;color=%23B1440E&amp;ctz=America%2FLos_Angeles" style=" border: 0;" width="105%" height="550px" frameborder="0" scrolling="no"></iframe>
+                </div>
+			</div>
+			<br>
+			<div class="post">
+				<h2 class="title"><a>Did we miss something?</a></h2>
+				<div class="entry">
+					<p>E-mail us at <a href="mailto:contactus@teammeanmachine.org">contactus@teammeanmachine.org</a> to suggest another event or to request a change to an event.</p>
+				</div>
+			</div>
