da0c87d7b402cfdc87ff1b73fee1e39d0c1f3d50
HiceS <shawnhice@gmail.com>
2015-05-30 10:16:49 -0700

    Fixed Calendar
    Fixed the Calendar that broke due to google changing the way they format
    embedded links once again. I expect this came from the IO conference,
    but this problem was tested across 3 servers to check for consistency
    and was proven to be hosts fault and not a developer error.

--- content/calendar_content.php
+++ content/calendar_content.php
@@ -2,7 +2,8 @@
 				<h1 class="title"><a href="#">Calendar</a></h1>
 				<div class="entry">
 <!--					<iframe src="http://www.google.com/calendar/embed?src=chsfirst%40gmail.com&amp;ctz=America/Los_Angeles" style="border: 0;" frameborder="0" height="550" scrolling="no" width="100%"></iframe>-->
-                    <iframe src="jquery/restylegc/restylegc.php?src=chsfirst%40gmail.com&color=%234E5D6C&showTitle=0&showTabs=0&showCalendars=0" style="border: 0;" frameborder="0" height="550px" scrolling="no" width="105%"></iframe>
+             <!--       <iframe src="jquery/restylegc/restylegc.php?src=chsfirst%40gmail.com&color=%234E5D6C&showTitle=0&showTabs=0&showCalendars=0" style="border: 0;" frameborder="0" height="550px" scrolling="no" width="105%"></iframe> -->
+                    <iframe src="https://www.google.com/calendar/embed?height=550&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=chsfirst%40gmail.com&amp;color=%23B1440E&amp;ctz=America%2FLos_Angeles" style=" border: 0;" width="105%" height="550px" frameborder="0" scrolling="no"></iframe>
                 </div>
 			</div>
 			<br>
