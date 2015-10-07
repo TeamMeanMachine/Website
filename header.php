846c0ce69cd684fd2f1ffa621251759f55da21d1
Ethroes <bmurray738@gmail.com>
2015-10-07 13:10:07 -0700

    Changed Header

--- header.php
+++ header.php
@@ -1,15 +1,15 @@
 <div id="header">
     <table id="logo">
         <tr>
-            <td id="headerleft">
+            <td id="headermiddle">
             </td>
             <td id="headermiddle">
                 <img src="images/MeanMachinelogo90px.png" alt="Mean Machine Logo" />
-                <h1><a href="index.php">FIRST Team 2471 Mean Machine</a></h1>
+                <h1><a href="index.php">Team Mean Machine 2471</a></h1>
                 <br>
-                <p><b>Robotics from Camas, Washougal, and Hockinson High Schools in Washington State</b></p>
+                <p><b>FRC Robotics from Camas, Washougal, and Hockinson High Schools in Washington State</b></p>
             </td>
-            <td id="headerright">
+            <td id="headermiddle">
             </td>
         </tr>
     </table>
