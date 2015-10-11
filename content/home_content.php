0147f77d9da07fd9d42912c6d7c877e34adc7440
Ethroes <bmurray738@gmail.com>
2015-10-10 21:47:42 -0700

    Updated Upcoming Events

--- content/home_content.php
+++ content/home_content.php
@@ -6,10 +6,6 @@
 <h1>Upcoming Events:</h1>
 <ul>
   <br>
-  <li>FRC Business Round Table - October 3rd, 2015</li>
-  <br>
-  <li>Girl's Generation - October 10th, 2015</li>
-  <br>
   <li>FIRST Fair - October 24th, 2015</li>
   <br>
   <li>Skyridge FLL Tournament - December 5th, 2015</li>
