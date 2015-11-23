c629eba3e178aee30404780778b9e3280922ba22
Ethroes <bmurray738@gmail.com>
2015-11-23 13:08:06 -0800

    Update home_content.php

--- content/home_content.php
+++ content/home_content.php
@@ -1,7 +1,10 @@
 
+<h1>Feature Story:</h1>
 <div width="100%">
    <iframe style="display: block; margin: 0 auto;" width="560" height="315" src="https://www.youtube.com/embed/jKCi7nu20BU" frameborder="0" allowfullscreen></iframe>
 </div>
+<br>
+<h3>2471 at USD</h3>
 <br>
 <h1>Upcoming Events:</h1>
 <ul>
