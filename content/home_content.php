0b820b5dc483898a21c7dbda91f50e77233bc264
Ethroes <bmurray738@gmail.com>
2016-01-11 16:27:51 -0800

    Changed home page titles

--- content/home_content.php
+++ content/home_content.php
@@ -1,10 +1,8 @@
 
-<h1>Feature Story:</h1>
+<h1>2015 Unveil Video:</h1>
 <div width="100%">
    <iframe style="display: block; margin: 0 auto;" width="560" height="315" src="https://www.youtube.com/embed/jKCi7nu20BU" frameborder="0" allowfullscreen></iframe>
 </div>
-<br>
-<h3>2471 at USD</h3>
 <br>
 <h1>Upcoming Events:</h1>
 <ul>
