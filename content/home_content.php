bd7e876f231458ef42de1317a7821e29ec83ee68
Ethroes <bmurray738@gmail.com>
2016-01-16 23:20:34 -0800

    Fixed bag and tag date
    Referenced Calendar(inaccurate) and not FIRST website(accurate).

--- content/home_content.php
+++ content/home_content.php
@@ -9,7 +9,7 @@
   <br>
   <li>Chairman's & Woodie Flowers submissions due - February 4th, 2016</li>
   <br>
-  <li>Bag Day - February 18th, 2016</li>
+  <li>Bag Day - February 23rd, 2016</li>
   <br>
   <li>PNW Wilsonville District Event Load In - March 10th, 2016</li>
   <br>
