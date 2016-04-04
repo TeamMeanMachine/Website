680b3580328c45e1afc49b74e1e12984a94a06de
Ethroes <bmurray738@gmail.com>
2016-04-03 19:20:07 -0700

    Updated Home Page, added video

--- content/home_content.php
+++ content/home_content.php
@@ -1,18 +1,18 @@
 
+<h1>Carbon Knight at PNW CAIS District Event:</h1>
+<div width="100%">
+   <iframe style="display: block; margin: 0 auto;" width="560" height="315" src="https://www.youtube.com/watch?v=ri74l07qRAA&list=PLwES-Sbpsnxw5tt0cEcpBqGUQsdynl_ii&index=79" frameborder="0" allowfullscreen></iframe>
+</div>
+<br>
+<br>
 <h1>2016 Unveil Video:</h1>
 <div width="100%">
    <iframe style="display: block; margin: 0 auto;" width="560" height="315" src="https://youtu.be/OIRAUX4TTXc" frameborder="0" allowfullscreen></iframe>
 </div>
 <br>
+<br>
 <h1>Upcoming Events:</h1>
 <ul>
-  <br>
-  <li>Easter Egg Hunt Demo - March 27th, 2016</li>
-  <br>
-  <li>PNW Oregon City District Event Load In - March 31th, 2016</li>
-  <br>
-  <li>PNW Oregon City District Event - April 1st-April 2st, 2016</li>
-  <br>
   <li>PNW District Championship Load In - April 6th, 2016</li>
   <br>
   <li>PNW District Championship - April 7th-April 9th, 2016</li>
@@ -22,7 +22,6 @@
   <li>FRC World Championship - April 28th-April 30th, 2016</li>
   <br>
   <li>FRC World Championship Load Out - May 1st, 2016</li>
-  
 </ul>
 <br>
 <p><u><a href="http://team2471.org/hours/">Build Season Hours</a></u></p>
