925e9667852406c18f46ae776647debbd74077dd
Brian Murray <brian@murraytwins.com>
2016-04-23 13:12:19 -0700

    Fix hrefs in media_content.php

--- content/media_content.php
+++ content/media_content.php
@@ -5,55 +5,55 @@
 				</div>
 			</div>
 			<div class="post">
-				<h2 class="title"><a href="#">2016 Unveil Video:</a></h2>
+				<h2 class="title" id="2016Unveil"><a href="#2016Unveil">2016 Unveil Video:</a></h2>
 				<div class="entry">
 					<iframe width="560" height="315" src="https://www.youtube.com/embed/OIRAUX4TTXc" frameborder="0" allowfullscreen></iframe>
 				</div>
 			</div>
 			<div class="post">
-				<h2 class="title"><a href="#">2015 Unveil Video:</a></h2>
+				<h2 class="title" id="2015Unveil"><a href="#2015Unveil">2015 Unveil Video:</a></h2>
 				<div class="entry">
 					<iframe width="560" height="315" src="https://www.youtube.com/embed/jKCi7nu20BU" frameborder="0" allowfullscreen></iframe>
 				</div>
 			</div>
 			<div class="post">
-				<h2 class="title"><a href="#">2014 Unveil Video:</a></h2>
+				<h2 class="title" id="2014Unveil"><a href="#2014Unveil">2014 Unveil Video:</a></h2>
 				<div class="entry">
 					<iframe src="//www.youtube.com/embed/3dkIj91HrF0" width="560" height="315" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 				</div>
 			</div>
 			<div class="post">
-				<h2 class="title"><a href="#">2013 Ultimate Ascent:</a></h2>
+				<h2 class="title" id="2013UltAsc"><a href="#2013UltAsc">2013 Ultimate Ascent:</a></h2>
 				<div class="entry">
 					<iframe src="//player.vimeo.com/video/60426670" width="560" height="315" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 				</div>
 			</div>
 			<div class="post">
-				<h2 class="title"><a href="#">Team 2471 RoboBlast (2012):</a></h2>
+				<h2 class="title" id="2012RoboBlast"><a href="#2012RoboBlast">Team 2471 RoboBlast (2012):</a></h2>
 				<div class="entry">
 					<iframe width="560" height="315" src="http://www.youtube.com/embed/KhJT_jlSuNw" frameborder="0" allowfullscreen></iframe>
 				</div>
 			</div>
 			<div class="post">
-				<h2 class="title"><a href="#">Team 2471 Portland Regional (2012):</a></h2>
+				<h2 class="title" id="2012Regional"><a href="#2012Regional">Team 2471 Portland Regional (2012):</a></h2>
 				<div class="entry">
 					<iframe width="560" height="315" src="http://www.youtube.com/embed/1V3VylRTlFg" frameborder="0" allowfullscreen></iframe>
 				</div>
 			</div>
 			<div class="post">
-				<h2 class="title"><a href="#">Team 2471 (2011):</a></h2>
+				<h2 class="title" id="2011"><a href="#2011">Team 2471 (2011):</a></h2>
 				<div class="entry">
 					<iframe width="560" height="315" src="http://www.youtube.com/embed/-hTyFODcNSQ" frameborder="0" allowfullscreen></iframe>
 				</div>
 			</div>
 			<div class="post">
-				<h2 class="title"><a href="#">Team 2471 (2009):</a></h2>
+				<h2 class="title" id="2009"><a href="#2009">Team 2471 (2009):</a></h2>
 				<div class="entry">
 					<iframe width="560" height="315" src="http://www.youtube.com/embed/IbUViGsX6R4" frameborder="0" allowfullscreen></iframe>
 				</div>
 			</div>
 			<div class="post">
-				<h2 class="title"><a href="#">Team 2471 (2008):</a></h2>
+				<h2 class="title" id="2008"><a href="#2008">Team 2471 (2008):</a></h2>
 				<div class="entry">
 					<iframe width="420" height="315" src="http://www.youtube.com/embed/p-8s-QmKfmg" frameborder="0" allowfullscreen></iframe>
 				</div>
